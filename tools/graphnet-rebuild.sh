47eab1ac819c6fde28f466ffa8fa531b311b9172
Niko Patterson <npatt@proton.me>
2026-05-28 09:29:57 -0400

    bump graphnet to current master commit

---
+++ tools/graphnet-rebuild.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# load environment.txt
+if ! [ -f ./environment.txt ]; then
+    echo "ERROR: Unable to read environment.txt, are you at the repository root?" >> /dev/stderr;
+    exit 1;
+fi;
+source ./environment.txt
+
+# rebuild graphnet
+pushd external/graphnet
+    "$PIP_EXECUTABLE" install --ignore-installed --no-binary graphnet -e '.[develop,torch-27]' -f https://data.pyg.org/whl/torch-2.7.0+cu128.html || exit 1;
+popd
