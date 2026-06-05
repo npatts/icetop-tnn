493b54a93c3de767b5b1a36dce8fecc7b3e96a9e
Niko Patterson <npatt@proton.me>
2026-06-05 13:48:53 -0400

    add(datagen): sort input data for graphnet

--- icetoptnn/data/cli.py
+++ icetoptnn/data/cli.py
@@ -4,11 +4,13 @@
 
 from argparse import ArgumentParser, Namespace;
 from os import pardir, system
-import sys;
 from tempfile import TemporaryDirectory;
 from pathlib import Path;
+import sys;
+import gzip;
 
 from graphnet.data.dataconverter import DataConverter;
+from graphnet.data.extractors.icecube import I3TruthExtractor
 from graphnet.data.readers import I3Reader;
 from graphnet.data.writers import ParquetWriter;
 from graphnet.data.extractors.icecube.i3genericextractor import I3GenericExtractor;
@@ -17,6 +19,24 @@ from .. import environment, util;
 
 ap_root_parser:   ArgumentParser;
 ap_create_parser: ArgumentParser;
+
+KNOWN_EXTENSIONS = [ '.i3', '.i3.gz', '.i3.bz2', '.root', '.h5' ];
+SKIPPED_EXTENSIONS = [ '.root', '.h5' ];
+I3FILE_EXTENSIONS = [ '.i3', '.i3.gz', '.i3.bz2' ];
+
+class SubDataset:
+    """IceTop sub-dataset"""
+
+    # Composition
+    composition: str;
+
+    # Energy levels
+    energy_min: float;
+    energy_max: float;
+
+    # Events
+    # (Path path, str extension, str layout)
+    events: list[tuple[Path, str, str]] = [];
 
 def apply_arguments(subparsers) -> None:
     """Apply arguments to the data subcommand"""
@@ -35,14 +55,13 @@ def apply_arguments(subparsers) -> None:
                                   default='./data/')
     ap_create_parser.add_argument(metavar='input', type=Path, help='input directories', dest='data_create_inputs', nargs='+',
                                   default='./data.i3')
-    ap_create_parser.add_argument('-G', type=Path, dest='data_create_gcd',
-                                  help='default gcd file, defaults to a non-existent placeholder',
-                                  default='/@/invalid/no-gcd-specified');
+    ap_create_parser.add_argument('-G', type=Path, dest='data_create_recoverygcd',
+                                  help='Use the specified GCD as a placeholder if a event has no associated GCD');
 
     return
 
 def main(args: Namespace) -> None:
-    """Data subcommand entry point"""
+    """`data` subcommand entry point"""
     global ap_root_parser;
 
     match args.data_subcommand:
@@ -54,6 +73,8 @@ def main(args: Namespace) -> None:
             exit(1);
 
 def sub_create(args: Namespace) -> None:
+    """`data create` subcommand entry point"""
+
     # Validate arguments
     for file in args.data_create_inputs:
         if not file.exists():
@@ -69,15 +90,19 @@ def sub_create(args: Namespace) -> None:
             exit(1);
     elif not args.data_create_output.parent.exists():
         print(f'error: "{args.data_create_output.parent}" is not a directory');
+        exit(1);
     else:
         args.data_create_output.mkdir(parents=False);
+
+    if args.data_create_recoverygcd is not None and not args.data_create_recoverygcd.exists():
+        print(f'error: placeholder gcd "{args.data_create_recoverygcd}" does not exist');
+        exit(1);
 
     if sum(1 for _ in args.data_create_output.iterdir()) != 0:
         if not util.prompt_yn(f'The output directory "{args.data_create_output}" already exists. Continue?'):
             exit(1);
 
-    # Path path, str extension, str layout
-    events: list[tuple[Path, str, str]] = []
+    subdatasets: dict[str, SubDataset] = {}
 
     # str layout -> Path path, str extension
     gcds: dict[str, tuple[Path, str]] = {}
@@ -87,62 +112,115 @@ def sub_create(args: Namespace) -> None:
     for root in args.data_create_inputs:
         for dir, _, files in root.walk():
             for file in files:
-                name: str;
-                ext: str;
-                if file.endswith('.i3.gz'):
-                    ext = 'gz';
-                    name = file[:-6];
-                elif file.endswith('.i3.bz2'):
-                    ext = 'bz2';
-                    name = file[:-7];
-                elif file.endswith('.root') or file.endswith('.h5'): # ignore whatever these are
-                    continue
-                else:
-                    raise Exception(f'Unexpected file extension on {file}');
+                (name, ext) = get_ext(file, KNOWN_EXTENSIONS);
+                if ext in SKIPPED_EXTENSIONS:
+                    continue;
 
-                # split into components by the delimiter _
-                comps: list[str] = name.split('_');
-
-                match comps:
+                match name.split('_'):
                     case ['Level3', layout, 'GCD']:
                         gcds[layout] = (dir/file, ext);
                     case ['Level3', 'IC86.2012', 'SIBYLL2.1', composition, layout, event]:
-                        events.append((dir/file, ext, layout));
+                        take_event(subdatasets, dir.parent.name, dir/file, ext, layout, composition, float(dir.name));
                     case ['Level3', 'IC86.2012', 'SIBYLL2.1', composition, 'thinned', layout, event]:
-                        pass; # skip thinned
-                    case ['Level3', 'IC86.2012', 'SIBYLL2.1', composition, layout, _, _]: # what are these? the second to last is probably energy? what's the last thing?
-                        events.append((dir/file, ext, layout));
+                        pass;
+                    case ['Level3', 'IC86.2012', 'SIBYLL2.1', composition, layout, energyraw, energylevelevent]:
+                        if energyraw[0] != 'E': raise Exception(f'Unrecognized file name pattern {file}');
+                        take_event(subdatasets, dir.name, dir/file, ext, layout, composition, float(energyraw[1:]));
                     case ['Level3', 'IC86.2012', 'SIBYLL2.1', composition, 'thinned', layout, _, _ ]:
                         pass; # skip thinned
                     case _:
                         raise Exception(f'Unrecognized file name pattern {file}');
 
+    # Remove any sub-datasets we don't want.
+    for name, sds in list(subdatasets.items()):
+        # Remove low/high energy sets, just in case we missed one.
+        if sds.energy_min < 5.0 or sds.energy_max > 8.0: del subdatasets[name];
+
+    for name, ds in subdatasets.items():
+        print(name);
+        print(ds.composition);
+        print(ds.energy_min, ds.energy_max);
+
     # "Validate" events
-    for path, _, layout in events:
-        if not layout in gcds:
-            raise Exception(f'No GCD for layout {layout} (event path: {path})');
+    for sds_name, sds in subdatasets.items():
+        for path, _, layout in sds.events:
+            if not layout in gcds:
+                if args.data_create_recoverygcd is not None:
+                    _, ext = get_ext(args.data_create_recoverygcd.name, I3FILE_EXTENSIONS);
+
+                    gcds[layout] = (args.data_create_recoverygcd, ext);
+                else:
+                    raise Exception(f'No GCD for layout {layout} (event path: {path})');
 
     # Feedback
-    print(f'Got {len(events)} events');
+    # print(f'Got {len(events)} events');
 
-    with TemporaryDirectory() as merged:
+    with TemporaryDirectory('.icetop-tnn-datagen') as merged:
         # Create layout directories
         for layout, (path, ext) in gcds.items():
             (Path(merged)/layout).mkdir();
-            (Path(merged)/layout/f'layout.gcd.i3.{ext}').symlink_to(path);
+            bind(Path(merged)/layout/f'layout.gcd{ext}', path);
 
         # Populate layout directories
         seq = 0;
-        for path, ext, layout in events:
-            (Path(merged)/layout/f'{seq}.i3.{ext}').symlink_to(path);
-            seq += 1;
+        for sds_name, sds in subdatasets.items():
+            for path, ext, layout in sds.events:
+                bind(Path(merged)/layout/f'{seq}{ext}', path)
+                seq += 1;
 
         system(f'ls -la {merged}');
 
-        DataConverter(
-            file_reader = I3Reader(gcd_rescue=str(args.data_create_gcd)),
-            save_method = ParquetWriter(),
-            outdir = str(args.data_create_output),
-            extractors = [ I3GenericExtractor() ]
-        )([merged]);
+        input()
 
+        try:
+            # Hand off to GraphNeT
+            DataConverter(
+                file_reader = I3Reader(gcd_rescue='/@/invalid/gcd-not-linked-you-should-never-see-this-something-is-very-very-wrong'),
+                save_method = ParquetWriter(),
+                outdir = str(args.data_create_output),
+                extractors = [ I3GenericExtractor() ]
+            )([merged]);
+        except Exception as e:
+            print(e);
+            input();
+            raise e;
+
+def take_event(subdatasets: dict[str, SubDataset], name: str,
+               path: Path, extension: str, layout: str,
+               composition: str, energy: float) -> None:
+    # Add new sub-dataset if one does not already exist.
+    if not name in subdatasets:
+        newds = SubDataset()
+        newds.composition = composition;
+        newds.energy_min = energy;
+        newds.energy_max = energy;
+        subdatasets[name] = newds;
+
+        print(f'Registered sub-dataset {name} {{ .c = {composition}, .emin = .emax = {energy} }}');
+    pass
+
+    # Add the event to the sub-dataset
+    if subdatasets[name].composition != composition: raise Exception(f'{subdatasets[name].composition} != {composition}');
+
+    subdatasets[name].events.append((path, extension, layout));
+    subdatasets[name].energy_min = min(subdatasets[name].energy_min, energy);
+    subdatasets[name].energy_max = max(subdatasets[name].energy_max, energy);
+
+def bind(path: Path, target: Path) -> None:
+    """Add a file to the datagen directory""";
+
+    if path.name.endswith('.i3'): # GraphNeT ignores i3files unless they're compressed.
+        with gzip.open(path.parent/(path.name+'.gz'), 'wb', 9) as out:
+            with open(target, 'rb') as src:
+                out.write(src.read());
+    else:
+        path.symlink_to(target);
+
+def get_ext(name: str, allowed: list[str]) -> tuple[str, str]:
+    """Split a file into it's name and extension using a list of allowed extensions"""
+
+    for ext in sorted(allowed, key=lambda a: -len(a)):
+        if (name.endswith(ext)):
+            return (name[:-len(ext)], ext);
+
+    raise Exception(f'Unexpected file extension on {name} (want {allowed})');
